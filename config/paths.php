4c8da93ca22ed18e75f375bf315b0e01353b43c7
kyaw phyo naing <iksan.kophyo@gmail.com>
2015-05-20 15:27:12 +0900

    gitignore update

--- config/paths.php
+++
@@ -1,5 +0,0 @@
-<?php
-//for local
-//define('URL', 'http://localhost/shoppingbox-mvc/');
-//for server
-define('URL', 'http://kyawphyonaing.dev.m-fr.net/shoppingbox-mvc/');
--- views/error/index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-//echo $this->msg; 
-echo "error";
-
-?>
